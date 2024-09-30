427e955215efd56ca8a2ef649ade21c138a29bf0
.hiszpvn <hiszpvnek@gmail.com>
2024-09-30 21:56:51 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author '.hiszpvn'
+
+ui_page {
+    'web/index.html'
+}
+
+client_scripts {
+    'client.lua',
+}
+
+files {
+    'web/script.js',
+    'web/styles.css',
+    'web/index.html',
+    'web/img/hiszpvn.png'
+}
